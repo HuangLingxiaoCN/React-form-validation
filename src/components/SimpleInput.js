d1a7b119061f6fb1d0b725f20df3819a62baf8b4
Lingxiao Huang <jasonhlx1213@gmail.com>
2021-08-26 19:06:03 +0300

    initial commit

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,69 @@
+// In this program we show two ways of reading user input
+// 1. Use state to monitor every key stokes
+// 2. Use ref to read input when submitting
+import { useState } from "react";
+
+const SimpleInput = (props) => {
+  // const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    // const enteredValue = nameInputRef.current.value;
+
+    setEnteredNameTouched(true);
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    console.log(enteredName);
+    setEnteredName("");
+    setEnteredNameTouched(false);
+  };
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          // ref={nameInputRef}
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
+      </div>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
